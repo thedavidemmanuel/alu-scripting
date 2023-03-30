930a127fd91c000e0651ab3e3efe6ae6dd4999c3
thedavidemmanuele <d.emmanuel@alustudent.com>
2023-03-30 13:41:01 +0000

    updates

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ 3-count.py """
+import json
 import requests
 
 
@@ -26,13 +27,27 @@ def count_words(subreddit, word_list, after="", count=[]):
 
         after = data['data']['after']
         if after is None:
-            result = [(word_list[i].lower(), count[i]) for i in range(len(word_list))]
-            result = sorted(result, key=lambda x: (-x[1], x[0]))
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
 
-            printed = set()
-            for word, c in result:
-                if c > 0 and word not in printed:
-                    print("{}: {}".format(word, c))
-                    printed.add(word)
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
+
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
             count_words(subreddit, word_list, after, count)
