1e246c2bafc51cc4ff1814aa57844dd09fd48817
thedavidemmanuele <d.emmanuel@alustudent.com>
2023-03-30 05:12:50 +0000

    api advanced

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
+    else:
+        return 0
