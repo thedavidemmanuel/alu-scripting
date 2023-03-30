fdf179b5d75280a06166bae857517a0bbdaca4c5
thedavidemmanuele <d.emmanuel@alustudent.com>
2023-03-30 12:41:45 +0000

    updates

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,47 +1,66 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given
-keywords.
+Recursive function that queries the Reddit API and returns a list containing
+the titles of all hot articles for a given subreddit.
 """
 import requests
 
 
-def count_words(subreddit, word_list, after='', count_dict={}):
-    """
-    Query Reddit API recursively, and prints a sorted count of given keywords
+def recurse(subreddit, hot_list=[], after=None):
+    """Return a list containing the titles of all hot articles for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/58.0.3029.110 Safari/537.36"}
+    params = {"limit": 100, "after": after}
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
 
-    Args:
-        subreddit: Name of the subreddit to be searched
-        word_list: List of keywords to count occurrences of
-        after: Pagination token to retrieve additional pages of the search
-        count_dict: Dictionary to store the count of each keyword
-
-    Returns:
-        Sorted count of given keywords
-    """
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'.format(subreddit, after)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 404:
+    if res.status_code != 200:
         return None
 
-    data = response.json().get('data')
-    children = data.get('children')
-    after = data.get('after')
+    res = res.json()
+    hot_posts = res.get("data").get("children")
+    for post in hot_posts:
+        hot_list.append(post.get("data").get("title"))
 
-    for child in children:
-        title = child.get('data').get('title').lower().split()
-
-        for word in word_list:
-            count_dict[word] = count_dict.get(word, 0) + title.count(word.lower())
-
+    after = res.get("data").get("after")
     if after is None:
-        sorted_words = sorted(count_dict.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_words:
-            if count == 0:
-                continue
-            print("{}: {}".format(word, count))
-        return sorted_words
+        return hot_list
 
-    return count_words(subreddit, word_list, after, count_dict)
+    return recurse(subreddit, hot_list, after)
+
+
+def count_words(subreddit, word_list, count_dict={}, after=None):
+    """Return a sorted count of given keywords for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/58.0.3029.110 Safari/537.36"}
+    params = {"limit": 100, "after": after}
+    res = requests.get(url, headers=headers, params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
+        return
+
+    res = res.json()
+    hot_posts = res.get("data").get("children")
+    for post in hot_posts:
+        words = post.get("data").get("title").lower().split()
+        for word in words:
+            if word in word_list:
+                if word.lower() in count_dict:
+                    count_dict[word.lower()] += 1
+                else:
+                    count_dict[word.lower()] = 1
+
+    after = res.get("data").get("after")
+    if after is None:
+        sorted_dict = dict(sorted(count_dict.items(),
+                                  key=lambda x: (-x[1], x[0])))
+        for k, v in sorted_dict.items():
+            print("{}: {}".format(k, v))
+        return
+
+    return count_words(subreddit, word_list, count_dict, after)
