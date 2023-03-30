9c8e8d412d21c183c0c0fc2f772490bee0b13d86
thedavidemmanuele <d.emmanuel@alustudent.com>
2023-03-30 05:34:20 +0000

    api advanced

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1-top_ten.py - Queries the Reddit API and prints the titles of the first 10 hot
+               posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        top = response.json().get('data').get('children')
+        for post in top[:10]:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
+
